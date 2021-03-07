eb648eb34ab615bed18dcf0236644f92dc012a1e
Mateusz <mat.lyczek@gmail.com>
2021-03-07 20:43:50 +0100

    Naprawa testu

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,7 +5,7 @@ describe('Test the root path', () =>{
     test('It should response the GET method', async()=>{
         try {
             const resp = await request(app).get('/')
-            expect(resp.staticCode).toBe(200)
+            expect(resp.statusCode).toBe(200)
             expect(resp.text).toBe('Hello World!')
         } catch(e){
             throw e
